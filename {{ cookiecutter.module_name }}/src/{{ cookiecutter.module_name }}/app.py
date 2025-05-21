b54dd2550932c5bbcb40b93dc1046f8245debb8d
Christopher Lackner <clackner@cerbsim.com>
2025-05-21 17:59:21 +0200

    fix heading string

--- {{ cookiecutter.module_name }}/src/{{ cookiecutter.module_name }}/app.py
+++ {{ cookiecutter.module_name }}/src/{{ cookiecutter.module_name }}/app.py
@@ -4,7 +4,7 @@ from webapp_client.components import *
 class {{ cookiecutter.app_class }}(App):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.title = Heading({{ cookiecutter.app_title }})
+        self.title = Heading("{{ cookiecutter.app_title }}")
         self.button = QBtn("Click me", ui_color="primary")
         self.counter_view = QInput(ui_model_value=0,
                                    ui_style="width: 200px;")
